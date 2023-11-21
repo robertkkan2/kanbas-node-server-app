e1a9aebe6749c75617591ebc52e7d0b2979db9f5
robertkkan <RobertKan2020@gmail.com>
2023-11-20 23:02:43 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
+const app = express();
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+app.use(express.json());
+AssignmentRoutes(app);
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
